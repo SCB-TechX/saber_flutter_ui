63e27e443a05976c392c9a76fc202e114938dbf4
Ayuth Mangmesap <ayuth.mang@gmail.com>
2022-08-10 10:21:01 +0700

    test: add more theme test

--- test/travel_wallet_ui_test.dart
+++ test/travel_wallet_ui_test.dart
@@ -4,13 +4,16 @@ import 'package:travel_wallet_ui/travel_wallet_ui.dart';
 
 void main() {
   group('TravelWallet themeData', () {
-    test('is defined for standard', () {
+    test(
+        'is defined for blackTheme, blueTheme, greenTheme, yellowTheme, purpleTheme, redTheme, blackAndBlueTheme',
+        () {
       expect(ThemeColor.blackTheme, isA<ThemeData>());
       expect(ThemeColor.blueTheme, isA<ThemeData>());
       expect(ThemeColor.greenTheme, isA<ThemeData>());
       expect(ThemeColor.yellowTheme, isA<ThemeData>());
       expect(ThemeColor.purpleTheme, isA<ThemeData>());
       expect(ThemeColor.redTheme, isA<ThemeData>());
+      expect(ThemeColor.blackAndBlueTheme, isA<ThemeData>());
     });
   });
 }
