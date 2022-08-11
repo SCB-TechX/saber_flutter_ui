7bd2a5dbf1a19747c8bd482c891b5833240c209d
Ayuth Mangmesap <ayuth.mang@gmail.com>
2022-08-11 17:26:25 +0700

    refactor: secondary button test

--- test/src/widgets/secondary_button_test.dart
+++ test/src/widgets/secondary_button_test.dart
@@ -5,140 +5,233 @@ import 'package:travel_wallet_ui/travel_wallet_ui.dart';
 
 void main() {
   final ThemeData blackTheme = ThemeColor.blackTheme;
-  final side = BorderSide(width: 1, color: blackTheme.primaryColor);
-  final shape = RoundedRectangleBorder(
+  final buttonStyle = <ButtonSize, EdgeInsets>{
+    ButtonSize.normal: getPadding(ButtonSize.normal),
+    ButtonSize.small: getPadding(ButtonSize.small),
+    ButtonSize.extraSmall: getPadding(ButtonSize.extraSmall),
+  };
+  final Map<ButtonSize, TextStyle> textStyle = {
+    ButtonSize.normal: getTextStyle(ButtonSize.normal),
+    ButtonSize.small: getTextStyle(ButtonSize.small),
+    ButtonSize.extraSmall: getTextStyle(ButtonSize.extraSmall),
+  };
+  final buttonShape = RoundedRectangleBorder(
     borderRadius: const BorderRadius.all(Radius.circular(24)),
-    side: side,
+    side: BorderSide(
+      width: 1,
+      color: blackTheme.primaryColor,
+    ),
   );
 
-  testWidgets('renders SecondaryButton', (WidgetTester tester) async {
-    bool wasPressed = false;
+  testWidgets('renders SecondaryButton default styles',
+      (WidgetTester tester) async {
+    ButtonSize size = ButtonSize.extraSmall;
     MaterialApp materialApp = MaterialApp(
       theme: blackTheme,
       home: Center(
           child: SecondaryButton(
-              onPressed: () {
-                wasPressed = true;
-              },
-              label: 'button')),
+        onPressed: () {},
+        label: 'button',
+        size: size,
+      )),
     );
 
     await tester.pumpWidget(materialApp);
 
-    Finder buttonMaterial = find.descendant(
+    Material buttonWidget = tester.widget<Material>(find.descendant(
       of: find.byType(SecondaryButton),
       matching: find.byType(Material),
+    ));
+    Padding paddingWidget = tester.widget<Padding>(
+      find.descendant(
+        of: find.byType(SecondaryButton),
+        matching: find.byType(Padding),
+      ),
     );
+    Text textWidget = tester.widget<Text>(find.descendant(
+        of: find.byType(SecondaryButton), matching: find.byType(Text)));
+    expect(buttonWidget.shape, buttonShape);
+    expect(paddingWidget.padding, buttonStyle[size]);
+    expect(textWidget.style!.fontFamily, textStyle[size]!.fontFamily);
+    expect(textWidget.style!.fontWeight, textStyle[size]!.fontWeight);
+    expect(textWidget.style!.fontSize, textStyle[size]!.fontSize);
 
-    Material material = tester.widget<Material>(buttonMaterial);
-
-    await tester.tap(buttonMaterial);
-    expect(wasPressed, true);
-    expect(
-        material.shape,
-        RoundedRectangleBorder(
-          borderRadius: const BorderRadius.all(Radius.circular(24)),
-          side: BorderSide(width: 1, color: blackTheme.primaryColor),
-        ));
-    expect(material.textStyle!.fontFamily, matches(Fonts.poppins));
-    expect(material.textStyle!.fontWeight, FontWeight.w600);
-    expect(material.textStyle!.fontSize,
-        materialApp.theme!.textTheme.button!.fontSize);
-
-    // onPress = null
+    // small
+    size = ButtonSize.small;
     materialApp = MaterialApp(
       theme: blackTheme,
-      home: const Center(
-          child: SecondaryButton(onPressed: null, label: 'button')),
+      home: Center(
+          child: SecondaryButton(
+        onPressed: () {},
+        label: 'button',
+        size: size,
+      )),
     );
+
     await tester.pumpWidget(materialApp);
 
-    buttonMaterial = find.descendant(
+    buttonWidget = tester.widget<Material>(find.descendant(
       of: find.byType(SecondaryButton),
       matching: find.byType(Material),
+    ));
+    paddingWidget = tester.widget<Padding>(
+      find.descendant(
+        of: find.byType(SecondaryButton),
+        matching: find.byType(Padding),
+      ),
+    );
+    textWidget = tester.widget<Text>(find.descendant(
+        of: find.byType(SecondaryButton), matching: find.byType(Text)));
+    expect(buttonWidget.shape, buttonShape);
+    expect(paddingWidget.padding, buttonStyle[size]);
+    expect(textWidget.style!.fontFamily, textStyle[size]!.fontFamily);
+    expect(textWidget.style!.fontWeight, textStyle[size]!.fontWeight);
+    expect(textWidget.style!.fontSize, textStyle[size]!.fontSize);
+
+    // extra small
+    size = ButtonSize.extraSmall;
+    materialApp = MaterialApp(
+      theme: blackTheme,
+      home: Center(
+          child: SecondaryButton(
+        onPressed: () {},
+        label: 'button',
+        size: size,
+      )),
     );
 
-    material = tester.widget<Material>(buttonMaterial);
+    await tester.pumpWidget(materialApp);
 
-    expect(material.textStyle!.fontFamily, matches(Fonts.poppins));
-    expect(material.textStyle!.fontWeight, FontWeight.w600);
-    expect(material.textStyle!.fontSize,
-        materialApp.theme!.textTheme.button!.fontSize);
-    expect(
-        material.shape,
-        const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(24)),
-          side: BorderSide(width: 1, color: SystemColors.disable),
-        ));
+    buttonWidget = tester.widget<Material>(find.descendant(
+      of: find.byType(SecondaryButton),
+      matching: find.byType(Material),
+    ));
+    paddingWidget = tester.widget<Padding>(
+      find.descendant(
+        of: find.byType(SecondaryButton),
+        matching: find.byType(Padding),
+      ),
+    );
+    textWidget = tester.widget<Text>(find.descendant(
+        of: find.byType(SecondaryButton), matching: find.byType(Text)));
+    expect(buttonWidget.shape, buttonShape);
+    expect(paddingWidget.padding, buttonStyle[size]);
+    expect(textWidget.style!.fontFamily, textStyle[size]!.fontFamily);
+    expect(textWidget.style!.fontWeight, textStyle[size]!.fontWeight);
+    expect(textWidget.style!.fontSize, textStyle[size]!.fontSize);
   });
 
-  testWidgets('renders SecondaryButtonWithIcon', (WidgetTester tester) async {
-    const IconData iconData = Icons.abc;
-    bool wasPressed = false;
+  testWidgets('renders SecondaryButtonWithIcon default styles',
+      (WidgetTester tester) async {
+    IconData iconData = Icons.abc;
+    ButtonSize size = ButtonSize.extraSmall;
     MaterialApp materialApp = MaterialApp(
-        theme: blackTheme,
-        home: Center(
+      theme: blackTheme,
+      home: Center(
           child: SecondaryButtonWithIcon(
-              onPressed: () {
-                wasPressed = true;
-              },
-              label: 'button',
-              icon: iconData),
-        ));
+        onPressed: () {},
+        label: 'button',
+        size: size,
+        icon: iconData,
+      )),
+    );
 
     await tester.pumpWidget(materialApp);
 
-    Finder buttonMaterial = find.descendant(
+    Material buttonWidget = tester.widget<Material>(find.descendant(
+      of: find.byType(SecondaryButtonWithIcon),
+      matching: find.byType(Material),
+    ));
+    Padding paddingWidget = tester.widget<Padding>(
+      find.descendant(
         of: find.byType(SecondaryButtonWithIcon),
-        matching: find.byType(Material));
-    Finder iconMaterial = find.byIcon(iconData);
+        matching: find.byType(Padding),
+      ),
+    );
+    Text textWidget = tester.widget<Text>(find.descendant(
+        of: find.byType(SecondaryButtonWithIcon), matching: find.byType(Text)));
+    Icon iconWidget = tester.widget(find.descendant(
+        of: find.byType(SecondaryButtonWithIcon), matching: find.byType(Icon)));
+    expect(buttonWidget.shape, buttonShape);
+    expect(paddingWidget.padding, buttonStyle[size]);
+    expect(textWidget.style!.fontFamily, textStyle[size]!.fontFamily);
+    expect(textWidget.style!.fontWeight, textStyle[size]!.fontWeight);
+    expect(textWidget.style!.fontSize, textStyle[size]!.fontSize);
+    expect(iconWidget.icon, iconData);
+    expect(iconWidget.size, 16.0);
 
-    Material material = tester.widget<Material>(buttonMaterial);
-    Icon targetIcon = tester.widget(iconMaterial);
-
-    await tester.tap(buttonMaterial);
-    expect(wasPressed, true);
-    expect(
-        material.shape,
-        RoundedRectangleBorder(
-          borderRadius: const BorderRadius.all(Radius.circular(24)),
-          side: BorderSide(width: 1, color: blackTheme.primaryColor),
-        ));
-    expect(material.textStyle!.fontFamily, matches(Fonts.poppins));
-    expect(material.textStyle!.fontWeight, FontWeight.w600);
-    expect(material.textStyle!.fontSize,
-        materialApp.theme!.textTheme.button!.fontSize);
-    expect(targetIcon.icon!.codePoint, iconData.codePoint);
-    expect(targetIcon.size, 16.0);
-
+    // small
+    size = ButtonSize.small;
     materialApp = MaterialApp(
-        theme: blackTheme,
-        home: const Center(
+      theme: blackTheme,
+      home: Center(
           child: SecondaryButtonWithIcon(
-              onPressed: null, label: 'button', icon: iconData),
-        ));
+        onPressed: () {},
+        label: 'button',
+        size: size,
+        icon: iconData,
+      )),
+    );
 
     await tester.pumpWidget(materialApp);
 
-    buttonMaterial = find.descendant(
+    buttonWidget = tester.widget<Material>(find.descendant(
+      of: find.byType(SecondaryButtonWithIcon),
+      matching: find.byType(Material),
+    ));
+    paddingWidget = tester.widget<Padding>(
+      find.descendant(
         of: find.byType(SecondaryButtonWithIcon),
-        matching: find.byType(Material));
-    iconMaterial = find.byIcon(iconData);
+        matching: find.byType(Padding),
+      ),
+    );
+    textWidget = tester.widget<Text>(find.descendant(
+        of: find.byType(SecondaryButtonWithIcon), matching: find.byType(Text)));
+    iconWidget = tester.widget(find.descendant(
+        of: find.byType(SecondaryButtonWithIcon), matching: find.byType(Icon)));
+    expect(buttonWidget.shape, buttonShape);
+    expect(paddingWidget.padding, buttonStyle[size]);
+    expect(textWidget.style!.fontFamily, textStyle[size]!.fontFamily);
+    expect(textWidget.style!.fontWeight, textStyle[size]!.fontWeight);
+    expect(textWidget.style!.fontSize, textStyle[size]!.fontSize);
+    expect(iconWidget.icon, iconData);
+    expect(iconWidget.size, 16.0);
 
-    material = tester.widget<Material>(buttonMaterial);
-    targetIcon = tester.widget(iconMaterial);
+    // extra small
+    size = ButtonSize.extraSmall;
+    materialApp = MaterialApp(
+      theme: blackTheme,
+      home: Center(
+          child: SecondaryButtonWithIcon(
+        onPressed: () {},
+        label: 'button',
+        size: size,
+        icon: iconData,
+      )),
+    );
 
-    expect(
-        material.shape,
-        const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(24)),
-          side: BorderSide(width: 1, color: SystemColors.disable),
-        ));
-    expect(material.textStyle!.fontFamily, matches(Fonts.poppins));
-    expect(material.textStyle!.fontWeight, FontWeight.w600);
-    expect(material.textStyle!.fontSize,
-        materialApp.theme!.textTheme.button!.fontSize);
-    expect(targetIcon.icon!.codePoint, iconData.codePoint);
-    expect(targetIcon.size, 16.0);
+    await tester.pumpWidget(materialApp);
+
+    buttonWidget = tester.widget<Material>(find.descendant(
+      of: find.byType(SecondaryButtonWithIcon),
+      matching: find.byType(Material),
+    ));
+    paddingWidget = tester.widget<Padding>(
+      find.descendant(
+        of: find.byType(SecondaryButtonWithIcon),
+        matching: find.byType(Padding),
+      ),
+    );
+    textWidget = tester.widget<Text>(find.descendant(
+        of: find.byType(SecondaryButtonWithIcon), matching: find.byType(Text)));
+    iconWidget = tester.widget(find.descendant(
+        of: find.byType(SecondaryButtonWithIcon), matching: find.byType(Icon)));
+    expect(buttonWidget.shape, buttonShape);
+    expect(paddingWidget.padding, buttonStyle[size]);
+    expect(textWidget.style!.fontFamily, textStyle[size]!.fontFamily);
+    expect(textWidget.style!.fontWeight, textStyle[size]!.fontWeight);
+    expect(textWidget.style!.fontSize, textStyle[size]!.fontSize);
+    expect(iconWidget.icon, iconData);
+    expect(iconWidget.size, 16.0);
   });
 }
