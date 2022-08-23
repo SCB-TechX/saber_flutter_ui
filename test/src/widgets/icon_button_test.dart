3f84c9fa62704544c6026cbf624b54efd5796838
Ayuth Mangmesap <ayuth.mang@gmail.com>
2022-08-23 12:14:12 +0700

    fix: icon button its argument :wrench:

---
+++ test/src/widgets/icon_button_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:travel_wallet_ui/travel_wallet_ui.dart';
+
+void main() {
+  group('getButtonShape', () {
+    test('returns circle shape', () {
+      // expect(getButtonShape(ButtonShape.circle), const CircleBorder());
+    });
+
+    test('returns rectangle shape', () {
+      // expect(getButtonShape(ButtonShape.circle), const CircleBorder());
+    });
+  });
+}
