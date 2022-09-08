2a3d4d81337f4943664c3db210dbd8eb5a3382b8
Ayuth Mangmesap <ayuth.mang@gmail.com>
2022-09-08 16:48:45 +0700

    add register page

---
+++ example/investment_app_demo/lib/pages/register.dart
@@ -0,0 +1,200 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:investment_app_demo/pages/signin.dart';
+import 'package:saber_flutter_ui/saber_flutter_ui.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _emailField = TextEditingController();
+  final TextEditingController _passwordField = TextEditingController();
+  final TextEditingController _confirmPasswordField = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _passwordVisible = false;
+  bool _confirmPasswordVisible = false;
+
+  @override
+  void dispose() {
+    _emailField.dispose();
+    _passwordField.dispose();
+    _confirmPasswordField.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: const Color(0xFF222222),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Image.asset('assets/images/saber_labs_logo_white.png'),
+            ),
+            Expanded(
+              flex: 3,
+              child: Form(
+                key: _formKey,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10))),
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Let’s create your account',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      const SizedBox(height: 24),
+                      TextFormField(
+                        controller: _emailField,
+                        decoration: const InputDecoration(
+                          labelText: "E-mail",
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(8))),
+                        ),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        controller: _passwordField,
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                        obscureText:
+                            !_passwordVisible, //This will obscure text dynamically
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: const OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(8))),
+                          suffix: GestureDetector(
+                            child: Icon(
+                              // Based on passwordVisible state choose the icon
+                              _passwordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Theme.of(context).primaryColorDark,
+                            ),
+                            onTap: () {
+                              // Update the state i.e. toggle the state of passwordVisible variable
+                              setState(() {
+                                _passwordVisible = !_passwordVisible;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        controller: _confirmPasswordField,
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                        obscureText: !_confirmPasswordVisible,
+                        decoration: InputDecoration(
+                          labelText: 'Confirm password',
+                          border: const OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(8))),
+                          suffix: GestureDetector(
+                            child: Icon(
+                              _confirmPasswordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Theme.of(context).primaryColorDark,
+                            ),
+                            onTap: () {
+                              setState(() {
+                                _confirmPasswordVisible =
+                                    !_confirmPasswordVisible;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 56),
+                      SizedBox(
+                        width: double.infinity,
+                        child: PrimaryButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              log(_emailField.text);
+                              log(_passwordField.text);
+                              log(_confirmPasswordField.text);
+
+                              // Navigator.push(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //         builder: (context) => const DashboardPage()));
+                            }
+                          },
+                          label: 'Create account',
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 13),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text('Already have an account? '),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 5),
+                                child: GestureDetector(
+                                  behavior: HitTestBehavior.opaque,
+                                  onTap: () => {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const SignInPage()))
+                                  },
+                                  child: const Text('Sign in',
+                                      style: TextStyle(
+                                          color: const Color(0xff222222),
+                                          fontWeight: FontWeight.w800)),
+                                ),
+                              )
+                            ]),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
